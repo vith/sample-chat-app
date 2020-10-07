16278a9b2a21f265e63b49975ce02704466fd11b
Jason Papakostas <vithos@gmail.com>
2020-10-07 17:03:18 +0200

    add fomantic-ui styles and react components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "fomantic-ui-css/semantic.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import { Button } from "semantic-ui-react";
+
+export default function Index() {
+  return <Button>fomantic component test</Button>;
+}
