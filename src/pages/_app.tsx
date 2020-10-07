d630a881d7d809b0503428d626b8a07c9e1bf450
Jason Papakostas <vithos@gmail.com>
2020-10-07 17:13:39 +0200

    enable typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "fomantic-ui-css/semantic.css";
-import "../styles/globals.css";
+import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
